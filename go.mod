d29ffe100c7f08141bf188abb974c86d7df9284c
Saso Matejina <8398242+iamsaso@users.noreply.github.com>
2023-09-19 15:16:33 -0400

    Update tags

---
+++ go.mod
@@ -0,0 +1,15 @@
+module example.com/datadog
+
+go 1.20
+
+require github.com/DataDog/datadog-api-client-go/v2 v2.17.0
+
+require (
+	github.com/DataDog/zstd v1.5.2 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	// Read Datadog API and Application Keys from environment variables
+	apiKey := os.Getenv("DATADOG_API_KEY")
+	appKey := os.Getenv("DATADOG_APPLICATION_KEY")
+
+	if apiKey == "" || appKey == "" {
+		fmt.Println("Datadog API and Application keys are not set in environment variables.")
+		return
+	}
+
+	ctx := context.WithValue(
+		context.Background(),
+		datadog.ContextAPIKeys,
+		map[string]datadog.APIKey{
+			"apiKeyAuth": {
+				Key: apiKey,
+			},
+			"appKeyAuth": {
+				Key: appKey,
+			},
+		},
+	)
+
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewUsageMeteringApi(apiClient)
+
+	optionalParams := datadogV1.NewGetUsageTopAvgMetricsOptionalParameters()
+	optionalParams.WithDay(time.Now().AddDate(0, 0, -1))
+	optionalParams.WithLimit(10)
+
+	resp, r, err := api.GetUsageTopAvgMetrics(ctx, *optionalParams)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `UsageMeteringApi.GetUsageTopAvgMetrics`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		return
+	}
+
+	// Loop through the metrics and get the active configuration for each
+	for _, metric := range resp.GetUsage() {
+		metricID := metric.GetMetricName()
+
+		configuration := datadog.NewConfiguration()
+		apiClient := datadog.NewAPIClient(configuration)
+		metricApiV2 := datadogV2.NewMetricsApi(apiClient)
+		metricApiV1 := datadogV1.NewMetricsApi(apiClient)
+
+		listActiveMetricConfigurationsOptionalParams := datadogV2.NewListActiveMetricConfigurationsOptionalParameters()
+		listActiveMetricConfigurationsOptionalParams.WithWindowSeconds(2628000)
+
+		metricResp, r, err := metricApiV2.ListActiveMetricConfigurations(ctx, metricID, *listActiveMetricConfigurationsOptionalParams)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.ListActiveMetricConfigurations`: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+
+		fmt.Println("-----------------------")
+		fmt.Println("Metric ID: ", *metricResp.Data.Id)
+
+		metricMetadataResp, r, err := metricApiV1.GetMetricMetadata(ctx, metricID)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.ListTagsByMetricName`: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+
+		fmt.Println("Metric Type: ", *metricMetadataResp.Type)
+
+		listTagConfigurationByNameResp, r, err := metricApiV2.ListTagConfigurationByName(ctx, metricID)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.ListTagConfigurationByName`: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+
+		if listTagConfigurationByNameResp.Data != nil {
+			body := datadogV2.MetricTagConfigurationUpdateRequest{
+				Data: datadogV2.MetricTagConfigurationUpdateData{
+					Type: datadogV2.METRICTAGCONFIGURATIONTYPE_MANAGE_TAGS,
+					Id:   metricID,
+					Attributes: &datadogV2.MetricTagConfigurationUpdateAttributes{
+						Tags: metricResp.Data.Attributes.ActiveTags,
+					},
+				},
+			}
+			updateTagConfigurationResp, r, err := metricApiV2.UpdateTagConfiguration(ctx, metricID, body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.UpdateTagConfiguration`: %v\n", err)
+				fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+			}
+
+			responseContent, _ := json.MarshalIndent(updateTagConfigurationResp, "", "  ")
+			fmt.Fprintf(os.Stdout, "Response from `MetricsApi.UpdateTagConfiguration`:\n%s\n", responseContent)
+		} else {
+			tag := datadogV2.MetricTagConfigurationCreateRequest{
+				Data: datadogV2.MetricTagConfigurationCreateData{
+					Type: datadogV2.METRICTAGCONFIGURATIONTYPE_MANAGE_TAGS,
+					Id:   metricID,
+					Attributes: &datadogV2.MetricTagConfigurationCreateAttributes{
+						Tags: metricResp.Data.Attributes.ActiveTags,
+						MetricType: datadogV2.MetricTagConfigurationMetricTypes(*metricMetadataResp.Type),
+					},
+				},
+			}
+	
+			createTagConfigurationnResp, r, err := metricApiV2.CreateTagConfiguration(ctx, metricID, tag)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.UpdateTagConfiguration`: %v\n", err)
+				fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+			}
+	
+			responseContent, _ := json.MarshalIndent(createTagConfigurationnResp, "", "  ")
+			fmt.Fprintf(os.Stdout, "Response from `MetricsApi.UpdateTagConfiguration`:\n%s\n", responseContent)
+		}
+	}
+}
