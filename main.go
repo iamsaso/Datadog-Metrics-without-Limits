bdd926810fdce5607595b42c87268295af45cb0c
Saso Matejina <8398242+iamsaso@users.noreply.github.com>
2023-09-20 13:48:59 -0400

    Add rate limiting

--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
 )
+
+type RateLimitInfo struct {
+    Limit     int
+    Name      string
+    Period    int
+    Remaining int
+    Reset     int
+}
 
 func main() {
 	// Read Datadog API and Application Keys from environment variables
@@ -40,10 +49,14 @@ func main() {
 	api := datadogV1.NewUsageMeteringApi(apiClient)
 
 	optionalParams := datadogV1.NewGetUsageTopAvgMetricsOptionalParameters()
+
+	// Set the start date for the metrics
 	optionalParams.WithDay(time.Now().AddDate(0, 0, -1))
-	optionalParams.WithLimit(10)
+	// Number of metrics to pull
+	optionalParams.WithLimit(100)
 
 	resp, r, err := api.GetUsageTopAvgMetrics(ctx, *optionalParams)
+	handleRateLimit(r)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `UsageMeteringApi.GetUsageTopAvgMetrics`: %v\n", err)
 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
@@ -63,27 +76,40 @@ func main() {
 		listActiveMetricConfigurationsOptionalParams.WithWindowSeconds(2628000)
 
 		metricResp, r, err := metricApiV2.ListActiveMetricConfigurations(ctx, metricID, *listActiveMetricConfigurationsOptionalParams)
+		handleRateLimit(r)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.ListActiveMetricConfigurations`: %v\n", err)
 			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+			continue
 		}
 
 		fmt.Println("-----------------------")
 		fmt.Println("Metric ID: ", *metricResp.Data.Id)
 
 		metricMetadataResp, r, err := metricApiV1.GetMetricMetadata(ctx, metricID)
+		handleRateLimit(r)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.ListTagsByMetricName`: %v\n", err)
 			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 		}
 
-		fmt.Println("Metric Type: ", *metricMetadataResp.Type)
+		metricType := datadogV2.METRICTAGCONFIGURATIONMETRICTYPES_DISTRIBUTION
+		if metricMetadataResp.Type != nil {
+			metricType = datadogV2.MetricTagConfigurationMetricTypes(*metricMetadataResp.Type)
+			fmt.Println("Metric Type: ", metricType)
+		}
 
 		listTagConfigurationByNameResp, r, err := metricApiV2.ListTagConfigurationByName(ctx, metricID)
+		handleRateLimit(r)
 		if err != nil {
+			fmt.Println("List Tag configuration not found. Creating new one.")
 			fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.ListTagConfigurationByName`: %v\n", err)
 			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 		}
+
+		fmt.Println("Metric Active Tags: ", metricResp.Data.Attributes.ActiveTags)
+
+		tagConfigurationnResp := datadogV2.MetricTagConfigurationResponse{}
 
 		if listTagConfigurationByNameResp.Data != nil {
 			body := datadogV2.MetricTagConfigurationUpdateRequest{
@@ -95,14 +121,12 @@ func main() {
 					},
 				},
 			}
-			updateTagConfigurationResp, r, err := metricApiV2.UpdateTagConfiguration(ctx, metricID, body)
+			tagConfigurationnResp, r, err = metricApiV2.UpdateTagConfiguration(ctx, metricID, body)
+			handleRateLimit(r)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.UpdateTagConfiguration`: %v\n", err)
 				fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 			}
-
-			responseContent, _ := json.MarshalIndent(updateTagConfigurationResp, "", "  ")
-			fmt.Fprintf(os.Stdout, "Response from `MetricsApi.UpdateTagConfiguration`:\n%s\n", responseContent)
 		} else {
 			tag := datadogV2.MetricTagConfigurationCreateRequest{
 				Data: datadogV2.MetricTagConfigurationCreateData{
@@ -110,19 +134,34 @@ func main() {
 					Id:   metricID,
 					Attributes: &datadogV2.MetricTagConfigurationCreateAttributes{
 						Tags: metricResp.Data.Attributes.ActiveTags,
-						MetricType: datadogV2.MetricTagConfigurationMetricTypes(*metricMetadataResp.Type),
+						MetricType: metricType,
 					},
 				},
 			}
 	
-			createTagConfigurationnResp, r, err := metricApiV2.CreateTagConfiguration(ctx, metricID, tag)
+			tagConfigurationnResp, r, err = metricApiV2.CreateTagConfiguration(ctx, metricID, tag)
+			handleRateLimit(r)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.UpdateTagConfiguration`: %v\n", err)
 				fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 			}
-	
-			responseContent, _ := json.MarshalIndent(createTagConfigurationnResp, "", "  ")
-			fmt.Fprintf(os.Stdout, "Response from `MetricsApi.UpdateTagConfiguration`:\n%s\n", responseContent)
 		}
+
+		fmt.Println("Metric Tags: ", tagConfigurationnResp.Data.Attributes.Tags)
 	}
+}
+
+func handleRateLimit(r *http.Response) {
+    if r != nil {
+        //limit, _ := strconv.Atoi(r.Header.Get("X-Ratelimit-Limit"))
+        //name := r.Header.Get("X-Ratelimit-Name")
+        //period, _ := strconv.Atoi(r.Header.Get("X-Ratelimit-Period"))
+        remaining, _ := strconv.Atoi(r.Header.Get("X-Ratelimit-Remaining"))
+        reset, _ := strconv.Atoi(r.Header.Get("X-Ratelimit-Reset"))
+
+		if remaining == 0 {
+			fmt.Println("Rate Limit Reached. Sleeping for ", reset, " seconds.")
+			time.Sleep(time.Duration(reset) * time.Second)
+		}
+    }
 }
